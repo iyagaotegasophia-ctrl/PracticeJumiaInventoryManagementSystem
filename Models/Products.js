9dd448b4f944c77ad063e75bf1600f1562b73ea4
Iyaga Otega Sophia <iyagaotegasophia@gmail.com>
2026-08-07 14:03:35 +0100

    myfirstpush

---
+++ Models/Products.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    timestamps: true //Date created and updated at
+});
+
+//create model from schema
+const Product = mongoose.model('Product', productSchema);
---
+++ Models/Users.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    Email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    hasAtmCard: {
+        type: Boolean,
+        default: false
+    },
+    
+    timestamps = true //Date Created, modified and updated at
+
+});
+
+//Create model from schema
+const User = mongoose.model('User', userSchema);
